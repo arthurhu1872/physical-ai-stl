33c7208c9242a2986d39840b36a8a3c3295b6a68
arthurhu1872 <arthurhuva@gmail.com>
2025-08-15 19:07:11 -0500

    Update __init__.py

--- src/physical_ai_stl/monitors/__init__.py
+++ src/physical_ai_stl/monitors/__init__.py
@@ -1,3 +1,3 @@
 """Example monitor package with STL and STREL hello functions."""
 
-__all__ = ["rtamt_hello", "moonlight_hello"]
+__all__ = ["rtamt_hello", "moonlight_hello", "moonlight_strel_hello"]
