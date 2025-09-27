42f5fc2a2616ddeb84e4bc260f0bd1eaf2d621e3
arthurhu1872 <arthurhuva@gmail.com>
2025-09-27 13:20:04 -0500

    Update __init__.py

--- src/physical_ai_stl/models/__init__.py
+++ src/physical_ai_stl/models/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from collections.abc import Callable, Iterable, Mapping
 import importlib
+from collections.abc import Callable, Iterable, Mapping
 from typing import Any, TYPE_CHECKING
 
 # ------------------------------------------------------------------------------
